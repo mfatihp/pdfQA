f53b4e702f9b86db9d0d6781192771bb59eba3f9
mfatihp <mfatihp@users.noreply.github.com>
2025-06-20 11:05:55 +0300

    remove chromadb_api and merge it into chat api

--- backend/chat_api.py
+++ backend/chat_api.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
-from Utils.librarian_llm import LibrarianLLM
+from fastapi import FastAPI, UploadFile
+from utils.librarian_llm import LibrarianLLM
+from utils.librarian_db import LibrarianDB
 
 
 app = FastAPI()
-librarian = LibrarianLLM()
+
+librarian_llm = LibrarianLLM()
+librarian_db = LibrarianDB()
 
 
-@app.get("/")
-def answer_question(user_message:str):
-    response = librarian.respond(text_input=user_message)
+
+@app.post("/answer")
+async def answer_question(user_message): #TODO: decide input type 
+    """
+    Creates llm response using RAG for user 
+    """
+    response = librarian_llm.respond(text_input=user_message)
     return response
+
+
+
+@app.post("/upload")
+async def upload_pdf(pdf_file): #TODO: add proper file upload and test it 
+    """
+    Uploads pdf files into local chromadb
+    """
+    librarian_db.add_documents_to_collection(pdf_file=pdf_file)
