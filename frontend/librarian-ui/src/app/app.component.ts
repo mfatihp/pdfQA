1d7b58dc29a3304a49e942e185d040198c849869
mfatihp <mfatihp@users.noreply.github.com>
2025-06-24 11:40:09 +0300

    feat: add preview pdf feature and autoscroll in chat

--- frontend/librarian-ui/src/app/app.component.ts
+++ frontend/librarian-ui/src/app/app.component.ts
@@ -2,21 +2,41 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, PdfViewerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
+  @ViewChild('chatMessages') private chatMessagesRef!: ElementRef;
   constructor(private http: HttpClient) {}
 
   title = 'librarian-ui';
   userInput: string = '';
   messages: { sender: 'user' | 'bot'; text: string }[] = [];
+
+  pdfSrc: string | Uint8Array | null = null;
+  zoom: number = 1.0;
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    try {
+      this.chatMessagesRef.nativeElement.scrollTop = this.chatMessagesRef.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error('Scroll failed:', err);
+    }
+  }
 
   sendMessage() {
     const messageText = this.userInput.trim();
@@ -24,6 +44,7 @@ export class AppComponent {
 
     this.messages.push({ sender: 'user', text: messageText });
     this.userInput = '';
+    this.scrollToBottom();
 
     const text = messageText;
 
@@ -31,11 +52,39 @@ export class AppComponent {
     .subscribe({
       next: (response) => {
         this.messages.push({ sender: 'bot', text: response.reply });
+        setTimeout(() => this.scrollToBottom(), 100);
       },
       error: (err) => {
         this.messages.push({ sender: 'bot', text: 'Error: could not get a reply.' });
         console.error(err);
+        setTimeout(() => this.scrollToBottom(), 100);
       }
     });
+  }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+
+    if (file && file.type === 'application/pdf') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.pdfSrc = new Uint8Array(e.target.result);
+      };
+
+      reader.readAsArrayBuffer(file);
+    } else {
+      this.pdfSrc = null;
+      alert('Lütfen sadece PDF dosyası seçiniz.');
+    }
+  }
+
+  afterLoadComplete(pdf: any): void {
+    console.log('PDF başarıyla yüklendi.')
+  }
+
+  onUploadClick() {
+    // Use it to send pdf to backend
+    console.log('Upload clicked');
   }
 }
